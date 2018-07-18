cef6b179895d93620a0b59394fd62f6b2bd2a9ac
mjablonska <martawaldon@gmail.com>
2018-07-18 09:25:47 +0200

    Added text to carousel

--- js/script.js
+++ js/script.js
@@ -6,24 +6,38 @@ $("#prev").click(function() {
         
         $('#two').removeClass('active-img').addClass('previous-img');
         
+        $('.text-two').addClass('hidde');
+        
         $('#three').removeClass('next-img').addClass('active-img-three');
+        
+        $('.text-three').removeClass('hidde');
         
     } else if ($('#two').hasClass('previous-img')) { //if three is active
        
         $('#one').removeClass('next-img-one').addClass('active-img-one');
         
+        $('.text-one').removeClass('hidde');
+        
         $('#two').removeClass('previous-img').addClass('next-img');
         
         $('#three').removeClass('active-img-three').addClass('previous-img');
+        
+        $('.text-three').addClass('hidde');
+
         
     } else if ($('#three').hasClass('previous-img')) { //if one is active
         
         $('#one').removeClass('active-img-one').addClass('previous-img');
         
+        $('.text-one').addClass('hidde');
+        
         $('#two').removeClass('next-img').addClass('active-img');
         
+        $('.text-two').removeClass('hidde');
+        
         $('#three').removeClass('previous-img').addClass('next-img');
-        };    
+        };   
+
 });
 
 $("#next").click(function() {
@@ -32,28 +46,50 @@ $("#next").click(function() {
         
         $('#one').removeClass('previous-img').addClass('active-img-one');
         
+        $('.text-one').removeClass('hidde');
+        
         $('#two').removeClass('active-img').addClass('next-img');
+        
+        $('.text-two').addClass('hidde');
         
         $('#three').removeClass('next-img').addClass('previous-img');
         
     } else if ($('#two').hasClass('next-img')) { //if one is active
 
         $('#three').removeClass('previous-img').addClass('active-img-three');
+        
+        $('.text-three').removeClass('hidde');
        
         $('#two').removeClass('next-img').addClass('previous-img'); 
         
         $('#one').removeClass('active-img-one').addClass('next-img-one'); 
+        
+        $('.text-one').addClass('hidde');
    
     } else if ($('#three').hasClass('active-img-three')) { //if three is active
 
         $('#three').removeClass('active-img-three').addClass('next-img');
+        
+        $('.text-three').addClass('hidde');
        
-        $('#two').removeClass('previous-img').addClass('active-img'); 
+        $('#two').removeClass('previous-img').addClass('active-img');
+        
+         $('.text-two').removeClass('hidde');
         
         $('#one').removeClass('next-img-one').addClass('previous-img'); 
     };
-
+    
 });
+
+
+
+
+
+
+
+
+
+
 
 
     
